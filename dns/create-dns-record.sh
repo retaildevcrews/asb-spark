d5dcc5f61ea95e0bd006dd6d83ae40577146a034
Akinlolu Akindele <aakindel@microsoft.com>
2021-06-09 14:36:43 -0500

    check if required variables are present before making dns call

--- dns/create-dns-record.sh
+++ dns/create-dns-record.sh
@@ -9,22 +9,29 @@ DNS_ZONE_RG=TLD
 # name of DNS Zone for the hack
 DNS_ZONE_NAME=aks-sb.com
 
-# TODO:
-#   exit early if either ip address or name config is missing
-
 # team name to be used for dns record
 # TODO:
 #   get team name from somewhere.
-#   currently planning on using a txt file that is checked into the branch.
-ASB_TEAM_NAME=gh-actions-test-4
+#   currently planning on using a txt file that is checked into the branch. looking for other options.
+ASB_TEAM_NAME=$(cat dns-name.txt)
 
 # public IP address of app gateway
 # TODO:
 #   get public ip address from somewhere.
-#   currently planning on using a txt file that is checked into the branch.
-APP_GW_PIP=0.0.0.0
+#   currently planning on using a txt file that is checked into the branch. looking for other options.
+APP_GW_PIP=$(cat public-ip-address.txt)
 
-# TODO: exit early if either "APP_GW_PIP" or "ASB_TEAM_NAME" variable is empty
+if [ -z "$ASB_TEAM_NAME" ]
+then
+  echo "ASB_TEAM_NAME name is required"
+  exit
+fi
+
+if [ -z "$APP_GW_PIP" ]
+then
+  echo "APP_GW_PIP is required"
+  exit
+fi
 
 # create the dns record
 az network dns record-set a add-record -g $DNS_ZONE_RG -z $DNS_ZONE_NAME -n $ASB_TEAM_NAME -a $APP_GW_PIP --query fqdn
--- setup.sh
+++ setup.sh
@@ -190,8 +190,11 @@ kubectl config rename-context $ASB_AKS_NAME $ASB_TEAM_NAME
 # save dns configs for this deployment.
 # TODO:
 #   what are other options for getting this data to the github action?
+#   maybe use the .env file that the user checks in to their branch?
 #   the action needs ASB_TEAM_NAME and the public ip address of the gateway.
-# echo $ASB_TEAM_NAME > dns/dns-name.txt
-# echo $ASB_AKS_PIP > dns/public-ip-address.txt
+rm -f dns/dns-name.txt
+echo $ASB_TEAM_NAME > dns/dns-name.txt
+rm -f dns/public-ip-address.txt
+echo $ASB_AKS_PIP > dns/public-ip-address.txt
 
 echo "add DNS A record - $ASB_TEAM_NAME  $ASB_AKS_PIP"
